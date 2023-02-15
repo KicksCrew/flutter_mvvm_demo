1702069e52b048f20eca98f07daf80f310751bd1
john chen <john.chen@kickscrew.com>
2023-02-15 15:44:42 +0800

    fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '/views/bus_Stops_view.dart';
+import '/views/bus_stops_view.dart';
 
 main() => runApp(App());
 
